3465b47b0ac60956b89fd7138037025e37fbcc3f
cookblue <dica.alderete@gmail.com>
2020-04-05 12:31:43 -0500

    feat(video):add component Video

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,20 +2,16 @@ import React from 'react'
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import ChatCard from './ChatCard';
+import Video from './Video'
 
 const useStyles = makeStyles({
   videoContainer: {
     position: 'relative',
     width: '100%',
     height: '92vh',
-    background: 'grey',
+    background: 'black',
     display: 'flex',
-    flexDirection: 'row'
   },
-  video: {
-    height: '92vh',
-    position: 'relative'
-  }
 })
 
 const VideoContainer = React.forwardRef(({ messages, srcVideo }, ref) => {
@@ -23,13 +19,9 @@ const VideoContainer = React.forwardRef(({ messages, srcVideo }, ref) => {
 
   return (
     <Box className={classes.videoContainer} >
-      <video controls className={classes.video} src={srcVideo} autoPlay={true}>
-        <source
-          src={srcVideo}
-          type="video/mp4" />
-      </video>
+      <Video srcVideo={srcVideo} />
       <Box style={{ position: 'absolute' }}>
-        <ChatCard messages={messages} ref={ref}/>
+        <ChatCard messages={messages} ref={ref} />
       </Box>
     </Box >
   )
