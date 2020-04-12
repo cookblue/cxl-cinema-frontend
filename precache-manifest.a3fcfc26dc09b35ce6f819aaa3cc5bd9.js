93dd326222bb38a1b6007ca43d1bc5588e539225
Luis Cairampoma <luis.cairampoma.arbildo@gmail.com>
2020-04-11 22:11:44 -0500

    Updates

---
+++ precache-manifest.a3fcfc26dc09b35ce6f819aaa3cc5bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac3682d810525a8dfa01d01d6ca8490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08d552e39877687b159",
+    "url": "/static/css/main.de401668.chunk.css"
+  },
+  {
+    "revision": "9da1b6fa022752072e8a",
+    "url": "/static/js/2.1e75c6f7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e75c6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08d552e39877687b159",
+    "url": "/static/js/main.1e133d75.chunk.js"
+  },
+  {
+    "revision": "5d2bc66a157ff61d0015",
+    "url": "/static/js/runtime-main.c1be022c.js"
+  }
+]);
